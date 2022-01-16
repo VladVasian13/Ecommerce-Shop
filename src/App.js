17517971cbe2e3ef96f4c1903f9a399c7885ce09
DESKTOP-725RJQK\Vlad <vlad.vasian96@gmail.com>
2022-01-16 14:28:53 +0200

    Testing Git is Working

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Everything
+          Learn React
         </a>
       </header>
     </div>
