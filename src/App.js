2e6aa05ccf28bed066b98e432e771ca2f4284de6
DESKTOP-725RJQK\Vlad <vlad.vasian96@gmail.com>
2022-01-20 13:34:11 +0200

    Creating SignIn Page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import { Routes,Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
-
+      <Header/>
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/shop' element={<ShopPage/>} />
+        <Route path='/signin' element={<SignInAndSignUpPage/>} />
       </Routes>
 
     </div>
