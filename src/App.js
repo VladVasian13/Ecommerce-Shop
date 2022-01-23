4c3e33ec91c9b36a99c9f79ac109fe24cae4d7fb
DESKTOP-725RJQK\Vlad <vlad.vasian96@gmail.com>
2022-01-23 18:29:43 +0200

    Adding Google Authentification With Firebase

--- src/App.js
+++ src/App.js
@@ -5,19 +5,43 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth, createUserProfileDocument } from './firebase/firbase.utils'
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/shop' element={<ShopPage/>} />
-        <Route path='/signin' element={<SignInAndSignUpPage/>} />
-      </Routes>
+class App extends React.Component {
+  
+  constructor() {
+    super();
 
-    </div>
-  );
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      // this.setState({currentUser: user});
+      createUserProfileDocument(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+    render() {
+      return (
+        <div>
+          <Header currentUser= {this.state.currentUser}/>
+          <Routes>
+            <Route path='/' element={<HomePage/>} />
+            <Route path='/shop' element={<ShopPage/>} />
+            <Route path='/signin' element={<SignInAndSignUpPage/>} />
+          </Routes>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firbase.utils.js
@@ -0,0 +1,52 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyA_drmLgX7QN7uUXpF15ufbrbs8udhVwA0",
+    authDomain: "ecommerce-db-8d6f1.firebaseapp.com",
+    projectId: "ecommerce-db-8d6f1",
+    storageBucket: "ecommerce-db-8d6f1.appspot.com",
+    messagingSenderId: "187069409223",
+    appId: "1:187069409223:web:6cc02f81bac4842d27dbe2"
+  };
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+    
+    return userRef;
+  }
+
+  firebase.initializeApp(config);
+  
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
+
+
+
